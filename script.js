009a25f38d4199e468c51a0c1097fe35f12c11ec
Showdownist <showdownist123@gmail.com>
2025-01-02 13:56:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let taskbar = document.getElementsByClassName("taskbar")[0]
-let startmenu = document.getElementsByClassName("startmenu")[0]
+const startMenu = document.querySelector('.startmenu');
+let isStartMenuOpen = false;
 
-taskbar.addEventListener("click", ()=>{
-    console.log("clicked");
-    if(startmenu.style.bottom == "50px"){
-        startmenu.style.bottom = "-655px"
+document.querySelector('.taskbar').addEventListener('click', () => {
+    if (isStartMenuOpen) {
+        // Hide the start menu
+        startMenu.style.bottom = '-655px'; // Move back below the screen
+        isStartMenuOpen = false;
+    } else {
+        // Show the start menu
+        startMenu.style.bottom = '60px'; // Position just above the taskbar
+        isStartMenuOpen = true;
     }
-    else{
-        startmenu.style.bottom = "50px"
-    }
-})
+});
